9bc25e5abbe2d4eee3a9fddee69d2c5ad2be0d31
unknown <varodhinipalacharla1998@gmail.com>
2026-03-19 18:23:56 +0530

    UC1: Application entry and welcome message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,20 @@
+/**
+ * Hotel Booking Management System
+ * Entry point of the application
+ * @author Vijay
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" Welcome to Book My Stay App");
+        System.out.println(" Hotel Booking Management System");
+        System.out.println(" Version: 1.0");
+        System.out.println("========================================");
+
+        System.out.println("System initialized successfully.");
+    }
+}
